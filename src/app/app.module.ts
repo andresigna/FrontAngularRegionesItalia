7c2c3797a9ec96a7a89c116f0a83fa8dbe7a97a7
andresigna <andresigna.ag@gmail.com>
2022-04-09 16:19:51 +0200

    Frontend de Regiones de Italia funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegionesComponent } from './regiones/regiones.component';
+import { RegionComponent } from './region/region.component';
+import { RegionesApiService } from './_services/regiones-api.service';
+import { ProvinciasApiService } from './_services/provincias-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegionesComponent,
+    RegionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RegionesApiService, ProvinciasApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
